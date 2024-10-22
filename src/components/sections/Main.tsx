38504e7c6e32a849633509d7c90e43821675e9c4
Jeovan Lindo <jeovanlindojr@gmail.com>
2024-10-22 16:56:32 -0300

    melhorando o link dos serviços vivo móvel e adicionando um link na seção main

--- src/components/sections/Main.tsx
+++ src/components/sections/Main.tsx
@@ -7,7 +7,7 @@ export default function Main({ id }: { id: string }){
       <section id={id} className="h-fit relative">
         <div className="flex absolute z-40 bottom-14 left-14">
           <button className="bg-white px-4 rounded-sm py-2 border text-fuchsia-700 hover:bg-gray-300 transition-all">
-            Veja nossas ofertas!
+            <a target="_blank" href='https://wa.me/55227242334?text=Ol%C3%A1,%20estou%20interessado%20em%20conhecer%20mais%20sobre%20os%20servi%C3%A7os%20da%20SmartLagos.%20Gostaria%20de%20saber%20mais%20sobre%20os%20planos%20dispon%C3%ADveis%20e%20como%20posso%20me%20tornar%20um%20cliente.%20Agrade%C3%A7o%20desde%20j%C3%A1%20pela%20aten%C3%A7%C3%A3o!'>Seja um cliente nosso!</a>
           </button>
         </div>
         <Carousel/>
